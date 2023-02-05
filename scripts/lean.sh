de736f12fad744dda843ca4444227e5a74837d78
gngpp <gngppz@gmail.com>
2023-02-05 17:37:25 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -9,11 +9,11 @@
 
 # 🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧
 
+# mt7921
 rm -rf package/kernel/rtl8821cu
 rm -rf package/kernel/mac80211
 rm -rf package/kernel/mt76
 rm -rf package/network/services/hostapd
-# openwrt 官方的只能使用ac模式
 svn export https://github.com/openwrt/openwrt/trunk/package/kernel/mac80211 package/kernel/mac80211
 svn export https://github.com/openwrt/openwrt/trunk/package/kernel/mt76 package/kernel/mt76
 svn export https://github.com/openwrt/openwrt/trunk/package/network/services/hostapd package/network/services/hostapd
