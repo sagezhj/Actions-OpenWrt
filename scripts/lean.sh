d4780d0f18abcbb0e842adf7f1e2471cac0912b2
gngpp <gngppz@gmail.com>
2023-02-05 13:58:20 +0800

    update

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -12,10 +12,11 @@
 rm -rf package/kernel/rtl8821cu
 rm -rf package/kernel/mac80211
 rm -rf package/kernel/mt76
+rm -rf package/network/services/hostapd
 # openwrt 官方的只能使用ac模式
 svn export https://github.com/openwrt/openwrt/trunk/package/kernel/mac80211 package/kernel/mac80211
 svn export https://github.com/openwrt/openwrt/trunk/package/kernel/mt76 package/kernel/mt76
-
+svn export https://github.com/openwrt/openwrt/trunk/package/network/services/hostapd package/network/services/hostapd
 
 # alist
 git clone https://github.com/DHDAXCW/luci-app-alist package/alist
