de84b71758b6640412ededc420324c5fbbb529e2
gngpp <gngppz@gmail.com>
2023-04-19 15:47:43 +0800

    Update

--- build/scripts/00_source_code.sh
+++ build/scripts/00_source_code.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 
-IMAGE="ghcr.io/gngpp/openwrt-build-cache:"
 if [ -n "$MODEL" ] && [ "$MODEL" == "r4se" ]; then 
-    IMAGE=$IMAGE:$MODEL_$VERSION
     git clone https://github.com/DHDAXCW/lede-rockchip openwrt;
 fi
 
 if [ -n "$MODEL" ] && [ "$MODEL" == "r5c" ]; then 
-    IMAGE=$IMAGE:$MODEL_$VERSION
     git clone https://github.com/DHDAXCW/lede-rockchip openwrt;
 fi
 
 if [ -n "$MODEL" ] && [ "$MODEL" == "x86" ]; then 
-    IMAGE=$IMAGE:$MODEL_$VERSION
     git clone https://github.com/coolsnowwolf/lede openwrt;
 fi
-
-export IMAGE
 
 cd openwrt
 rm -rf .git
