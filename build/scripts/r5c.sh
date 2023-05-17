a86b5cbf7c33f5a915ae32f7f28524c909810a86
gngpp <gngppz@gmail.com>
2023-05-17 11:33:29 +0800

    Update r5c.sh

--- build/scripts/r5c.sh
+++ build/scripts/r5c.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-rm -rf package/kernel/rtl8821cu
-
 # mt7921
+rm -rf package/kernel/rtl8821cu
+rm -rf package/kernel/mac80211
+rm -rf package/kernel/mt76
+rm -rf package/network/services/hostapd
 
-#rm -rf package/kernel/mac80211
-#rm -rf package/kernel/mt76
-#rm -rf package/network/services/hostapd
-
-#svn export https://github.com/openwrt/openwrt/trunk/package/kernel/mac80211 package/kernel/mt76
-#svn export https://github.com/openwrt/openwrt/trunk/package/kernel/mac80211 package/kernel/mac80211
-#svn export https://github.com/openwrt/openwrt/trunk/package/network/services/hostapd package/network/services/hostapd
+svn export https://github.com/openwrt/openwrt/trunk/package/kernel/mac80211 package/kernel/mt76
+svn export https://github.com/openwrt/openwrt/trunk/package/kernel/mac80211 package/kernel/mac80211
+svn export https://github.com/openwrt/openwrt/trunk/package/network/services/hostapd package/network/services/hostapd
 
 # Change default shell to zsh
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
